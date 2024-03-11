eeb1f9c74c9422a4d1e3fb1ba42a76c518719891
Drakkarrr <drakkar.alpha00@gmail.com>
2024-03-11 10:12:56 -0700

    refactor code to ts and modern cleaner more readable syntax

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,43 @@
-require('module-alias/register');
-const mongoose = require('mongoose');
-const { globSync } = require('glob');
-const path = require('path');
+import 'module-alias/register';
+import mongoose, { ConnectOptions } from 'mongoose';
+import { globSync } from 'glob';
+import path from 'path';
 
-// Make sure we are running node 7.6+
 const [major, minor] = process.versions.node.split('.').map(parseFloat);
 if (major < 20) {
-  console.log('Please upgrade your node.js version at least 20 or greater. 👌\n ');
+  console.log('Please upgrade your Node.js version to at least 20 or greater. 👌\n ');
   process.exit();
 }
 
-// import environmental variables from our variables.env file
-require('dotenv').config({ path: '.env' });
-require('dotenv').config({ path: '.env.local' });
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.local' });
 
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(
+  process.env.DATABASE as string,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions | any
+);
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY as string;
 
 mongoose.connection.on('error', (error) => {
   console.log(
-    `1. 🔥 Common Error caused issue → : check your .env file first and add your mongodb url`
+    `1. 🔥 Common Error caused issue → : check your .env file first and add your MongoDB URL`
   );
   console.error(`2. 🚫 Error → : ${error.message}`);
 });
 
-const modelsFiles = globSync('./src/models/**/*.js');
+const modelsFiles = globSync('./src/models/**/*.ts');
 
 for (const filePath of modelsFiles) {
   require(path.resolve(filePath));
 }
 
 // Start our app!
-const app = require('./app');
+import app from './app'; // Assuming app.js has been converted to TypeScript (app.ts)
 app.set('port', process.env.PORT || 8888);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → On PORT : ${server.address().port}`);
