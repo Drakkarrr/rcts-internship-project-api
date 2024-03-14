59624490b64fb5e3ac98ae4b2db4fa9cf389ce84
Drakkarrr <drakkar.alpha00@gmail.com>
2024-03-14 11:54:30 -0700

    added setup for admin seed data credentials to document model used to login admin

---
+++ src/setup/setup.ts
@@ -0,0 +1,100 @@
+import dotenv from 'dotenv';
+import { globSync } from 'glob';
+import fs from 'fs';
+import { generate as uniqueId } from 'shortid';
+import mongoose from 'mongoose';
+
+dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.local' });
+
+mongoose.connect(process.env.DATABASE!);
+
+interface AdminData {
+  email: string;
+  name: string;
+  surname: string;
+  enabled: boolean;
+  role: string;
+}
+
+interface AdminPasswordData {
+  password: string;
+  emailVerified: boolean;
+  salt: string;
+  user: mongoose.Types.ObjectId;
+}
+
+async function setupApp(): Promise<void> {
+  try {
+    const Admin = require('../models/coreModels/Admin');
+    const AdminPassword = require('../models/coreModels/AdminPassword');
+    const newAdminPassword = new AdminPassword();
+
+    const salt = uniqueId();
+
+    const passwordHash = newAdminPassword.generateHash(salt, 'admin123');
+
+    const demoAdmin: AdminData = {
+      email: 'admin@demo.com',
+      name: 'Admin',
+      surname: 'Test',
+      enabled: true,
+      role: 'owner',
+    };
+    const result = await new Admin(demoAdmin).save();
+
+    const AdminPasswordData: AdminPasswordData = {
+      password: passwordHash,
+      emailVerified: true,
+      salt: salt,
+      user: result._id,
+    };
+    await new AdminPassword(AdminPasswordData).save();
+
+    console.log('👍 Admin created : Done!');
+
+    const Setting = require('../models/coreModels/Setting');
+
+    const settingFiles: any[] = [];
+
+    const settingsFiles = globSync('./src/setup/defaultSettings/**/*.json');
+
+    for (const filePath of settingsFiles) {
+      const file = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+      settingFiles.push(...file);
+    }
+
+    await Setting.insertMany(settingFiles);
+
+    console.log('👍 Settings created : Done!');
+
+    const Currency = require('../models/appModels/Currency');
+    const { currencyList } = require('../utils/currencyList');
+    const PaymentMode = require('../models/appModels/PaymentMode');
+    const Taxes = require('../models/appModels/Taxes');
+
+    await Currency.insertMany(currencyList);
+    console.log('👍 Currency created : Done!');
+
+    await Taxes.insertMany([{ taxName: 'Tax 0%', taxValue: '0', isDefault: true }]);
+    console.log('👍 Taxes created : Done!');
+
+    await PaymentMode.insertMany([
+      {
+        name: 'Default Payment',
+        description: 'Default Payment Mode (Cash , Wire Transfert)',
+        isDefault: true,
+      },
+    ]);
+    console.log('👍 PaymentMode created : Done!');
+
+    console.log('🥳 Setup completed : Success!');
+    process.exit();
+  } catch (e) {
+    console.log('\n🚫 Error! The Error info is below');
+    console.log(e);
+    process.exit();
+  }
+}
+
+setupApp();
