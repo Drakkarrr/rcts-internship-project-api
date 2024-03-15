435cd67dec67841ad7c7f83d32a8e30bda12d264
Drakkarrr <drakkar.alpha00@gmail.com>
2024-03-15 12:31:20 -0700

    use Date format util for core setting app

---
+++ src/settings/useDate.ts
@@ -0,0 +1,11 @@
+const useDate = ({ settings }: { settings: { rfts_app_date_format: string } }) => {
+  const { rfts_app_date_format } = settings;
+
+  const dateFormat = rfts_app_date_format;
+
+  return {
+    dateFormat,
+  };
+};
+
+export default useDate;
