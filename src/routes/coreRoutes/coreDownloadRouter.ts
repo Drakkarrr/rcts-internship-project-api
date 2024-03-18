b2dd4a7a2a15fb5dcf94b56f257fe5177a059715
Drakkarrr <drakkar.alpha00@gmail.com>
2024-03-18 10:29:02 -0700

    file download router and file upload directory

---
+++ src/routes/coreRoutes/coreDownloadRouter.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { downloadPdf } from '@/handlers/downloadHandler/downloadPdf';
+
+const router = express.Router();
+import { hasPermission } from '@/middlewares/permission';
+
+router.route('/:directory/:file').get((req, res) => {
+  try {
+    const { directory, file } = req.params;
+    const id = file.slice(directory.length + 1).slice(0, -4);
+    downloadPdf(req, res, { directory, id });
+  } catch (error: string | any) {
+    return res.status(503).json({
+      success: false,
+      result: null,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
+export default router;
