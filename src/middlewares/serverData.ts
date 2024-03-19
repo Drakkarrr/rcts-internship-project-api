078517fbf7728229601c94ccb0eb0d9b11de2e16
Drakkarrr <drakkar.alpha00@gmail.com>
2024-03-19 11:36:08 -0700

    Server data middleware

---
+++ src/middlewares/serverData.ts
@@ -0,0 +1,24 @@
+import mongoose, { Document, Model } from 'mongoose';
+
+interface IDataResult extends Document {
+  removed: boolean;
+  enabled: boolean;
+}
+
+export const getData = <T extends IDataResult>({ model }: { model: string }): Promise<T[]> => {
+  const Model: Model<T> = mongoose.model<T>(model);
+  const result: Promise<T[]> = Model.find({ removed: false, enabled: true }).exec();
+  return result;
+};
+
+export const getOne = <T extends IDataResult>({
+  model,
+  id,
+}: {
+  model: string;
+  id: string;
+}): Promise<T | null> => {
+  const Model: Model<T> = mongoose.model<T>(model);
+  const result: Promise<T | null> = Model.findOne({ _id: id, removed: false }).exec();
+  return result;
+};
