1413551ea1a7cad97ef9a0bb80f832576d4ba8aa
Drakkarrr <drakkar.alpha00@gmail.com>
2024-03-19 12:33:31 -0700

    added entry point for settings middleware

---
+++ src/middlewares/settings/index.ts
@@ -0,0 +1,15 @@
+import loadSettings from './loadSettings';
+import listAllSettings from './listAllSettings';
+import listBySettingKey from './listBySettingKey';
+import readBySettingKey from './readBySettingKey';
+import updateBySettingKey from './updateBySettingKey';
+import increaseBySettingKey from './increaseBySettingKey';
+
+export {
+  loadSettings,
+  listAllSettings,
+  listBySettingKey,
+  readBySettingKey,
+  updateBySettingKey,
+  increaseBySettingKey,
+};
