7c3be44024376a1873fdbf6432be810e0adf37c6
Drakkarrr <drakkar.alpha00@gmail.com>
2024-03-21 01:02:02 -0700

    createdpayment controller for creating payment status and calculation

---
+++ src/controllers/appControllers/paymentController/create.ts
@@ -0,0 +1,85 @@
+import mongoose from 'mongoose';
+import { Request, Response } from 'express';
+import custom from '@/controllers/pdfController';
+import { calculate } from '@/helpers';
+
+const Model = mongoose.model('Payment');
+const Invoice = mongoose.model('Invoice');
+
+const create = async (req: Request | any, res: Response) => {
+  if (req.body.amount === 0) {
+    return res.status(202).json({
+      success: false,
+      result: null,
+      message: `The Minimum Amount couldn't be 0`,
+    });
+  }
+
+  const currentInvoice = await Invoice.findOne({
+    _id: req.body.invoice,
+    removed: false,
+  });
+
+  const {
+    total: previousTotal,
+    discount: previousDiscount,
+    credit: previousCredit,
+  } = currentInvoice;
+
+  const maxAmount = calculate.sub(calculate.sub(previousTotal, previousDiscount), previousCredit);
+
+  if (req.body.amount > maxAmount) {
+    return res.status(202).json({
+      success: false,
+      result: null,
+      message: `The Max Amount you can add is ${maxAmount}`,
+    });
+  }
+  req.body['createdBy'] = req.admin._id;
+  req.req.req.body['currency'] = currentInvoice.currency;
+
+  const result = await Model.create(req.body);
+
+  const fileId = 'payment-' + result._id + '.pdf';
+  const updatePath = await Model.findOneAndUpdate(
+    {
+      _id: result._id.toString(),
+      removed: false,
+    },
+    { pdf: fileId },
+    {
+      new: true,
+    }
+  ).exec();
+
+  const { _id: paymentId, amount } = result;
+  const { id: invoiceId, total, discount, credit } = currentInvoice;
+
+  let paymentStatus =
+    calculate.sub(total, discount) === calculate.add(credit, amount)
+      ? 'paid'
+      : Number(calculate.add(credit, amount)) > 0
+      ? 'partially'
+      : 'unpaid';
+
+  const invoiceUpdate = await Invoice.findOneAndUpdate(
+    { _id: req.body.invoice },
+    {
+      $push: { payment: paymentId.toString() },
+      $inc: { credit: amount },
+      $set: { paymentStatus: paymentStatus },
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  ).exec();
+
+  return res.status(200).json({
+    success: true,
+    result: updatePath,
+    message: 'Payment Invoice created successfully',
+  });
+};
+
+export default create;
