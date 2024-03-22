e9d7d780eeade3d538c293d5afe6a8d523b833bd
Drakkarrr <drakkar.alpha00@gmail.com>
2024-03-22 16:15:36 -0700

    fixed import module

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,5 +1,5 @@
-import { useDate } from './useDate';
-import { useMoney } from './useMoney';
-import { useAppSettings } from './useAppSettings';
+import useDate from './useDate';
+import useMoney from './useMoney';
+import useAppSettings from './useAppSettings';
 
 export { useDate, useMoney, useAppSettings };
