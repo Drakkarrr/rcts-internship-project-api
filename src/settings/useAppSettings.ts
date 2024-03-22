921a79d4d4581ad2e3980b77406ac0d1f95e8569
Drakkarrr <drakkar.alpha00@gmail.com>
2024-03-22 16:11:49 -0700

    changed app email and domain

--- src/settings/useAppSettings.ts
+++ src/settings/useAppSettings.ts
@@ -1,6 +1,6 @@
 export const useAppSettings = () => {
   let settings: { [key: string]: string } = {};
-  settings['rcts_app_email'] = 'noreply@rctsapp.com';
-  settings['rcts_base_url'] = 'https://cloud.rctsapp.com';
+  settings['rcts_app_email'] = 'noreply@responsivecodetechnologysolutions.com';
+  settings['rcts_base_url'] = 'https://cloud.responsivecodetechnologysolutions.com';
   return settings;
 };
