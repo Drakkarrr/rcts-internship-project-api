c1055f6fe3e31ed23dfc112574e998acc757664a
Drakkarrr <drakkar.alpha00@gmail.com>
2024-04-09 15:56:20 -0700

    happy bugging

--- src/controllers/coreControllers/adminAuth/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var createAuthMiddleware_1 = require("@/controllers/middlewaresControllers/createAuthMiddleware");
-exports.default = (0, createAuthMiddleware_1.default)('Admin');
